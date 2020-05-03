093d56882fe50a66306ee90be3cec83f7325322d
KingMartos <david.martos9.7@gmail.com>
2020-05-03 03:03:10 +0200

    Update driver_arduino.c

--- Cotxe/DRIVER/driver_arduino.c
+++ Cotxe/DRIVER/driver_arduino.c
@@ -1,27 +1,33 @@
-int ledi = 13;
-int ledd = 12;
+int ledi = 2;
+int ledd = 4;
+int leds = 12;
 void setup () {
-   pinMode(led, OUTPUT); //LED 13 como salida
+   pinMode(ledi, OUTPUT); //LED 13 como salida
+   pinMode(ledi, OUTPUT); //LED 13 como salida
    Serial.begin(9600); //Inicializo el puerto serial a 9600 baudios
+   digitalWrite(leds,HIGH);
 }
 
 /*
-	Este pequeño driver, de momento controla leds por tal de que se enciendan y se apaguen,
-	estos leds simulan a los motores del coche, de momento implementaremos tres funciones muy sencillas:
-	- ENcender el motor derecho.
-	- Encender el motor izquierdo.
-	- Detener los motores.
+  Este pequeño driver, de momento controla leds por tal de que se enciendan y se apaguen,
+  estos leds simulan a los motores del coche, de momento implementaremos tres funciones muy sencillas:
+  - ENcender el motor derecho.
+  - Encender el motor izquierdo.
+  - Detener los motores.
 */
 void loop () {
    if (Serial.available()) { //Si está disponible
       char c = Serial.read(); //Guardamos la lectura en una variable char
       if (c == 'D') { //Enciendo el motor derecho
+         digitalWrite(leds,LOW);
          digitalWrite(ledd, HIGH);
       } else if (c == 'I') { //Enciendo el motor izquierdo
-         digitalWrite(ledi, HIGH
+         digitalWrite(leds,LOW);
+         digitalWrite(ledi, HIGH);
       } else if (c == 'S') { //Apago los motores
          digitalWrite(ledi, LOW);
-	 digitalWrite(ledd, LOW);
+         digitalWrite(ledd, LOW);
+         digitalWrite(leds,HIGH);
       }
    }
 }
