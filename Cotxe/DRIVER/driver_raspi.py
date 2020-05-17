3c3ca5778a9b3f0a75e668ca4bacc9f4e3037def
KingMartos <david.martos9.7@gmail.com>
2020-05-17 20:33:24 +0200

    DRIVER FUNCIONAL
    Añadidos los controles para ir marcha atras y actualizada la descripcion de algunos comandos

--- Cotxe/DRIVER/driver_raspi.py
+++ Cotxe/DRIVER/driver_raspi.py
@@ -14,12 +14,17 @@ while True:
       arduino.write(comando) #Mandar un comando hacia Arduino
 	# Esta parte del codigo es prescindible, pero de momento la usaremos por tal de verificar el funcionamiento del mismo
       if comando == 'D':
-            print('ENCIENDE DERECHO')
-      elif comando == 'I':
-            print('ENCIENDE IZQUIERDO')
+            print('GIRO DERECHA')
+      elif comando == 'A':
+            print('GIRO IZQUIERDA')
       elif comando == 'S':
             print('APAGA MOTORES')
+      elif comando == 'X':
+            print('REVERSA')
+      elif comando == 'W':
+            print('HACIA DELANTE')
       elif comando == 'q':
+	    print('SALIENDO DEL CONTROLADOR')
 	    exit()
 
 arduino.close() #Finalizamos la comunicacion
